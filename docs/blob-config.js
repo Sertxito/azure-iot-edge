24bdf037e934195fc8bbe15d42b4bdeac4eb4327
Sertxito <shierroc@gmail.com>
2026-04-17 17:51:01 +0200

    feat(demo): landing municipal y command center smart city con rotacion

---
+++ docs/blob-config.js
@@ -0,0 +1,12 @@
+﻿window.SMARTCITY_BLOB_CONFIG = {
+  enabled: true,
+  baseUrl: "https://adls260406213642.blob.core.windows.net",
+  container: "iot-historical",
+  prefix: "aggregates/",
+  sasToken: "se=2026-04-24T15%3A01Z&sp=rl&spr=https&sv=2022-11-02&sr=c&skoid=8c31dba5-e534-4fa0-aa93-6af12cb96686&sktid=cf36141c-ddd7-45a7-b073-111f66d0b30c&skt=2026-04-17T15%3A01%3A19Z&ske=2026-04-24T15%3A01%3A00Z&sks=b&skv=2022-11-02&sig=OzuF6tiP9AzeEDdMZmbprJanxRqWF08XWVWf5TwIu8Q%3D",
+  maxBlobs: 12,
+  refreshMs: 45000,
+  panelRotateMs: 7000
+};
+
+
