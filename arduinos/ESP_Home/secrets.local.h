09a57ee3be5dfa124bbf1ca8297905565d249162
Sertxito <shierroc@gmail.com>
2026-04-18 10:44:58 +0200

    integrar marco operativo de agentes/skills en el repositorio

---
+++ arduinos/ESP_Home/secrets.local.h
@@ -0,0 +1,11 @@
+#ifndef WIFI_SSID_VALUE
+#define WIFI_SSID_VALUE "SertxitoTech"
+#endif
+
+#ifndef WIFI_PASS_VALUE
+#define WIFI_PASS_VALUE "g3stW1f1"
+#endif
+
+#ifndef MQTT_HOST_VALUE
+#define MQTT_HOST_VALUE "10.56.193.203"
+#endif
